925acc3962f2a4bc42a704a6654404264b0f2825
fawnyler <125075848+fawnyler@users.noreply.github.com>
2024-06-20 15:17:10 +0300

    final version

--- cardwidget.cpp
+++ cardwidget.cpp
@@ -5,12 +5,11 @@ cardwidget::cardwidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::cardwidget),
     m_number(0),
-    m_isOpened(false) // Инициализация переменных
+    m_isOpened(false),
+    m_isScored(false)
 {
     ui->setupUi(this);
     connect(ui->pushButton, &QPushButton::clicked, this, &cardwidget::on_cardButton_clicked);
-
-    // Изначально скрываем число на карточке
     ui->label->setText("");
 }
 
@@ -24,12 +23,31 @@ void cardwidget::setNumber(int number)
     m_number = number;
 }
 
+void cardwidget::showNumber(bool show)
+{
+    ui->label->setVisible(show);
+}
+
+void cardwidget::setScored(bool scored)
+{
+    m_isScored = scored;
+}
+
+bool cardwidget::isScored() const
+{
+    return m_isScored;
+}
+
 void cardwidget::on_cardButton_clicked()
 {
     if (!m_isOpened) {
-        // Если карточка не открыта, показываем число и отправляем сигнал
         ui->label->setText(QString::number(m_number));
         m_isOpened = true;
         emit cardClicked(m_number);
+
+        if (!m_isScored) {
+            m_isScored = true;
+            emit scoreCounted(m_number);
+        }
     }
 }
