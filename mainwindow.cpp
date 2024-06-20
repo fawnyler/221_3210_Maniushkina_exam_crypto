6896d269a6844a69601a1ff4a5abe77e88cb16d3
fawnyler <125075848+fawnyler@users.noreply.github.com>
2024-06-20 14:10:47 +0300

    summa

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,32 +1,72 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QRandomGenerator>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
-    , gameWindow(new GameWindow(this))
 {
     ui->setupUi(this);
 
-    connect(ui->edtPin, &QLineEdit::returnPressed, this, &MainWindow::on_edtPin_returnPressed);
+    // Set up login page
+    ui->editPin->setEchoMode(QLineEdit::Password);
+    ui->stackedWidget->setCurrentIndex(0);
+
+    // Set up game page
+    connect(ui->resetButton, &QPushButton::clicked, this, &MainWindow::resetGame);
 }
 
 MainWindow::~MainWindow()
 {
+    for (int i = 0; i < 9; ++i) {
+        delete m_cardWidgets[i];
+    }
+
     delete ui;
 }
 
-void MainWindow::on_btnLogin_clicked()
+void MainWindow::on_loginButton_clicked()
 {
-    if (ui->edtPin->text() == correctPin) {
-        gameWindow->show();
-        this->hide();
-    } else {
-        QMessageBox::warning(this, "Ошибка", "Неверный PIN");
+    QString enteredPin = ui->editPin->text();
+    
+    if (m_isStartup) {
+        if (enteredPin == m_correctPin) {
+            ui->stackedWidget->setCurrentIndex(1);
+            m_isStartup = false;
+            for (int i = 0; i < 9; ++i) {
+                m_cardWidgets[i] = new cardwidget(this);
+                ui->gridLayout->addWidget(m_cardWidgets[i], i / 3, i % 3);
+                int randomNumber = QRandomGenerator::global()->bounded(-100, 100);
+                m_cardWidgets[i]->setNumber(randomNumber);
+                connect(m_cardWidgets[i], &cardwidget::cardClicked, [=](int value) {
+                    m_score += value;
+                    ui->scoreLabel->setText("Очки: " + QString::number(m_score));
+                });
+            }
+        } else {
+            ui->lblLogin->setText("Неверный пинкод");
+            ui->lblLogin->setStyleSheet("color:red;");
+        }
+    }
+    
+    ui->editPin->clear();
+}
+
+
+void MainWindow::resetGame()
+{
+    m_score = 0;
+    ui->scoreLabel->setText("Очки: 0");
+
+    // Regenerate random numbers for each card
+    for (int i = 0; i < 9; ++i) {
+        int randomNumber = QRandomGenerator::global()->bounded(-100, 100);
+        m_cardWidgets[i]->setNumber(randomNumber);
     }
 }
 
-void MainWindow::on_edtPin_returnPressed()
-{
-    on_btnLogin_clicked();
-}
+
+
+
+
+
